0e13af3cdf1dbf015fc345a309b50d0b83c3314b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-26 22:14:17 +0900

    Create RemoteDataSourceModule.kt

---
+++ app/src/main/java/com/example/composegithubclient/model/remote_data_source/RemoteDataSourceModule.kt
@@ -0,0 +1,14 @@
+package com.example.composegithubclient.model.remote_data_source
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+class RemoteDataSourceModule {
+    @Provides
+    fun provideRemoteDataSource(remoteDataSourceImpl: RemoteDataSourceImpl) : RemoteDataSource
+        = remoteDataSourceImpl
+}
