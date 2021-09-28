5d7c2244f6731570c99718aac9641143e777546e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-09-28 22:17:56 +0900

    Update User.kt

--- app/src/main/java/com/example/composegithubclient/model/repository/User.kt
+++ app/src/main/java/com/example/composegithubclient/model/repository/User.kt
@@ -1,12 +1,11 @@
 package com.example.composegithubclient.model.repository
 
-import retrofit2.http.Url
 
 data class User(
     val userId: UserId,
     val name: String,
     val avatarImage: NetworkImage,
-    val blogUrl: com.example.composegithubclient.model.repository.Url
+    val blogUrl: Url
 )
 
 data class UserId(val value : Long)
