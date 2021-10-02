cb32d1c609be672c5b1db6a6a400b7d8ba5dacc1
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-02 23:49:01 +0900

    Update MainViewModel.kt

--- app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.composegithubclient.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.composegithubclient.model.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel {
+class MainViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
+
 }
