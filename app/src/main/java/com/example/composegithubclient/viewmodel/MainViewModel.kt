7f59093d85710357d32d5d0e4a736daa5f1d67e6
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-03 00:01:30 +0900

    Update MainViewModel.kt

--- app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
@@ -3,9 +3,11 @@ package com.example.composegithubclient.viewmodel
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.composegithubclient.model.repository.User
 import com.example.composegithubclient.model.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -23,6 +25,20 @@ class MainViewModel @Inject constructor(private val userRepository: UserReposito
         /** 読み込み失敗 */
         object Failure : UiState()
     }
-
     val uiState : MutableState<UiState> = mutableStateOf(UiState.Initial)
+    /** 検索フォームに入力された文字列を表すMutableState */
+    val searchQuery : MutableState<String> = mutableStateOf("")
+    fun onSearchTapped() {
+        val searchQuery = searchQuery.value
+        viewModelScope.launch {
+            uiState.value = UiState.Loading
+            runCatching {
+                userRepository.getUser(userName = searchQuery)
+            }.onSuccess {
+                uiState.value = UiState.Success(user = it)
+            }.onFailure {
+                uiState.value = UiState.Failure
+            }
+        }
+    }
 }
