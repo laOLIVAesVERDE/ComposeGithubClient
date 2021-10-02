4768b0313071e0e0a5646080fbc2ee2cdd4b5269
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-02 23:53:40 +0900

    Update MainViewModel.kt

--- app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/composegithubclient/viewmodel/MainViewModel.kt
@@ -1,11 +1,28 @@
 package com.example.composegithubclient.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.composegithubclient.model.repository.User
 import com.example.composegithubclient.model.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
+    /**
+     * Viewの状態を表すUiState
+     */
+    sealed class UiState {
+        /** 初期状態 */
+        object Initial : UiState()
+        /** 読み込み中 */
+        object Loading : UiState()
+        /** 読み込み成功 */
+        data class Success(val user : User) : UiState()
+        /** 読み込み失敗 */
+        object Failure : UiState()
+    }
 
+    val uiState : MutableState<UiState> = mutableStateOf(UiState.Initial)
 }
