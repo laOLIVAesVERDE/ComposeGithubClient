a4037636c21fcd8801d64f07b7ba45a564fc1f5e
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-03 10:35:37 +0900

    Create SearchView.kt

---
+++ app/src/main/java/com/example/composegithubclient/view/SearchView.kt
@@ -0,0 +1,9 @@
+package com.example.composegithubclient.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+
+@Composable
+fun SearchView(searchQuery : MutableState<String>, onSearchButtonTapped : () -> Unit) {
+    
+}
