a30c6ed9f741b0505536157bdba02fb63e6cef29
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-03 11:00:25 +0900

    Create ErrorView.kt

---
+++ app/src/main/java/com/example/composegithubclient/view/ErrorView.kt
@@ -0,0 +1,9 @@
+package com.example.composegithubclient.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ErrorView() {
+    Text(text = "読み込み失敗")
+}
