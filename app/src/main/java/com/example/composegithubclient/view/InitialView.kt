aa565e1f2aee32377f6a7f925a8bf08b117f6002
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-03 10:50:56 +0900

    Create InitialView.kt

---
+++ app/src/main/java/com/example/composegithubclient/view/InitialView.kt
@@ -0,0 +1,9 @@
+package com.example.composegithubclient.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InitialView() {
+    Text(text = "検索してください")
+}
