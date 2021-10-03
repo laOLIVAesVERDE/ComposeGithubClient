46d0a27c1f1833e1cebe9143c36d57fb6d10adaa
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2021-10-03 10:35:35 +0900

    Update MainView.kt

--- app/src/main/java/com/example/composegithubclient/view/MainView.kt
+++ app/src/main/java/com/example/composegithubclient/view/MainView.kt
@@ -1,10 +1,17 @@
 package com.example.composegithubclient.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import com.example.composegithubclient.viewmodel.MainViewModel
 
 @Composable
 fun MainView(mainViewModel: MainViewModel) {
     val uiState : MainViewModel.UiState by mainViewModel.uiState
+
+    Column(Modifier.fillMaxWidth()) {
+
+    }
 }
